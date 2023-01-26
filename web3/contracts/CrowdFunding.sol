1ed39557ed6030c6bcfa887f4cb5d4c966135fa6
Ankit <gptankit159@gmail.com>
2023-01-26 23:39:51 +0530

    Done with Solidity contract and Vite installation

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -14,9 +14,7 @@ contract CrowdFunding {
         uint256[] donations;
     }
 
-
     mapping(uint256 => Campaign) public campaigns;
-
     uint256 public numberOfCampaigns =0;
 
     function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
@@ -51,8 +49,15 @@ contract CrowdFunding {
     }
 
     function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
-        return ()
+        return ( campaigns[_id].donators, campaigns[_id].donations);
     }
 
-    function getCampaigns() {}
+    function getCampaigns() view public returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+        for(uint i =0;i< numberOfCampaigns;i++){
+            Campaign storage item = campaigns[i];
+            allCampaigns[i] = item;
+        }
+        return allCampaigns;
+    }
 }
