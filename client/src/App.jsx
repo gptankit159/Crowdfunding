2934d8d4d0830f7cdba980cd1d7f729c316406d6
Ankit <gptankit159@gmail.com>
2023-01-27 23:01:36 +0530

    Navbar Done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
-import {Home,CreateCampaign,Profile,CampaignDetails}  from './pages';
+import {Home, CreateCampaign,Profile,CampaignDetails}  from './pages';
 import {Navbar,Sidebar} from './components'
 
 
@@ -16,6 +16,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-campaign" element={<CreateCampaign />} />
+          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
         </Routes>
       </div>
     </div>
